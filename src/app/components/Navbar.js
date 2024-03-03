b1e7c59b4f0a61f2eb188c40f89923b7920b0a18
Naninara <nmvmanikanta@gmail.com>
2024-03-03 15:34:10 +0530

    navbar error fixed

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -9,7 +9,7 @@ export default async function Navbar() {
     <div className="gap-7 md:gap-0 text-xl md:text-base border-b-2 border-white flex-col justify-center items-center md:flex-row   flex md:justify-between p-[2%] font-semibold">
       <Link href={"/"}>Home Page</Link>
       <Link href={"/protected"}>Protected Page</Link>
-      <Link href={"/everyone"}>Everyone's Page</Link>
+      <Link href={"/everyone"}>Everyone Page</Link>
       {session ? (
         <Link href={"/api/auth/signout"}>
           <button>Logout</button>
